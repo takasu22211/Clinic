16a2fdf359e2aa2ba6e5457dc3e7c7652b63c2bb
joshuapilongco <95063228+joshuapilongco@users.noreply.github.com>
2021-11-26 22:43:37 -0700

    Ability to initialize Clinic object.

---
+++ Clinic.php
@@ -0,0 +1,49 @@
+<?php
+
+class Clinic {
+    private $name;
+    private $patients = [];
+
+    public function __construct($name) {
+        $this->name = $name;
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+
+    public function getAllPatients() {
+        return $this->patients;
+    }
+
+    public function getPatient($id) {
+        foreach ($this->patients as $patient) {
+            if ($patient->getId() == $id) {
+                return $patient;
+            }
+        }
+    }
+
+    public function setName($name) {
+        $this->name = $name;
+    }
+
+    public function addPatient(Patient $patient) {
+        array_push($this->patients, $patient);
+        Util::saveData();
+    }
+
+    public function deletePatient($id) {
+        $i = 0;
+        foreach ($this->patients as $patient) {
+            if ($patient->getId() == $id) {
+                array_splice($this->patients, $i, 1);
+                return true;
+            }
+
+            $i++;
+        }
+
+        return false;
+    }
+}
